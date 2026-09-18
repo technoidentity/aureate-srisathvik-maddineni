f474310ca9cd317997b9297350f893a7492dbf43
Srisathvik <srisathvikm@technoidentity.com>
2026-09-18 15:57:51 +0530

    Chapter 3.5 loops

---
+++ srisathvik-rust/loops/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let values = [10, 20, 30, 40, 50];
+
+    for element in values {
+        println!("The value is: {element}");
+    }
+
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+
+    println!("LIFTOFF!!!");
+}
