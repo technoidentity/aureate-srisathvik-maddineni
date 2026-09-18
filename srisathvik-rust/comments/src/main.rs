9bb0776dd17eef67032cb3ab70c7ec5d1ae88344
Srisathvik <srisathvikm@technoidentity.com>
2026-09-18 15:10:59 +0530

    Chapter 3.4 comments

---
+++ srisathvik-rust/comments/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // This comment is on its own line.
+    let lucky_number = 7; // This is an end-of-line comment.
+
+    // Each line of a multi-line explanation can begin with `//`.
+    // Rust ignores these lines when compiling the program.
+
+    /*
+       Rust also supports block comments.
+       Everything between these markers is ignored.
+    */
+
+    println!("My lucky number is: {lucky_number}");
+}
