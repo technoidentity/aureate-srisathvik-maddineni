0586da6f425312b799dee775bd8212b6f3e676b0
Srisathvik <srisathvikm@technoidentity.com>
2026-09-20 16:41:08 +0530

    Chapter 4.3 slice types

--- srisathvik-rust/ownership/src/main.rs
+++ srisathvik-rust/ownership/src/main.rs
@@ -1,11 +1,23 @@
 fn main() {
-    let message = create_message();
+    let sentence = "hello world";
+    let word = first_word(sentence);
 
-    println!("{message}");
+    println!("The first word is: {word}");
+
+    let numbers = [10, 20, 30, 40, 50];
+    let middle = &numbers[1..4];
+
+    println!("Array slice: {middle:?}");
 }
 
-fn create_message() -> String {
-    let message = String::from("hello");
+fn first_word(text: &str) -> &str {
+    let bytes = text.as_bytes();
 
-    message
+    for (index, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &text[..index];
+        }
+    }
+
+    text
 }
